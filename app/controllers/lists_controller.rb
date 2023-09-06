2594dd7513d412c51427fe84bf485a6d92326aa2
EC2 Default User <yuzugaia@users.noreply.github.com>
2023-09-06 02:50:30 +0000

    [finish]3-6

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -30,7 +30,7 @@ class ListsController < ApplicationController
   private
 
   def list_params
-    params.require(:list).permit(:title, :body)
+    params.require(:list).permit(:title, :body, :image)
   end
 
 end
